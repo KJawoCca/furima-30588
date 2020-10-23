19b8defde3ccadaf77599534a52177b7bf49edc9
KJawoCca <ykariya.pri@gmail.com>
2020-10-23 22:34:17 +0900

    手数料計算処理実装前

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,12 @@ class Item < ApplicationRecord
   belongs_to_active_hash :postage
   belongs_to_active_hash :province
   belongs_to_active_hash :number_of_day
-  
+
   belongs_to :user
   has_one_attached :image
 
   validates :image, :name, :text, presence: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :category, :condition, :postage, :province, :number_of_day, presence: true
   validates :category_id, :condition_id, :postage_id, :province_id, :number_of_day_id, numericality: { other_than: 1 }
 end

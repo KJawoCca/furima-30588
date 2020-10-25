d3ca0860dd940fe805c8d4edce087ca0f7ba7008
KJawoCca <ykariya.pri@gmail.com>
2020-10-25 21:42:27 +0900

    修正（1回目）

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -24,26 +24,51 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include("Text can't be blank")
     end
     it "カテゴリーの情報が必須であること" do
+      @item.category_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank")
+    end
+    it "カテゴリーの情報が選択されていること" do
       @item.category_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Category must be other than 1")
     end
     it "商品の状態についての情報が必須であること" do
+      @item.condition_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition can't be blank")
+    end
+    it "商品の状態についての情報が選択されていること" do
       @item.condition_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Condition must be other than 1")
     end
     it "配送料の負担についての情報が必須であること" do
+      @item.postage_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Postage can't be blank")
+    end
+    it "配送料の負担についての情報が選択されていること" do
       @item.postage_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Postage must be other than 1")
     end
     it "発送元の地域についての情報が必須であること" do
+      @item.province_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Province can't be blank")
+    end
+    it "発送元の地域についての情報が選択されていること" do
       @item.province_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Province must be other than 1")
     end
     it "発送までの日数についての情報が必須であること" do
+      @item.number_of_day_id = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Number of day can't be blank")
+    end
+    it "発送までの日数についての情報が選択されていること" do
       @item.number_of_day_id = 1
       @item.valid?
       expect(@item.errors.full_messages).to include("Number of day must be other than 1")
@@ -53,10 +78,12 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Price is not a number")
     end
-    it "価格の範囲が、¥300~¥9,999,999の間であること" do
+    it "価格が300円未満では登録できないこと" do
       @item.price = "299"
       @item.valid?
       expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+    end
+    it "価格が9,999,999円より高いと登録できないこと" do
       @item.price = "10000000"
       @item.valid?
       expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")

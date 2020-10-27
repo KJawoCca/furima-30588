4cb519ab9afb31594a6bf92fcd25db29e09cc5f7
KJawoCca <ykariya.pri@gmail.com>
2020-10-27 11:57:41 +0900

    修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: :new
-  before_action :remove, only: :edit
+  before_action :identify_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -20,15 +20,17 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
+    if user_signed_in? && (current_user.id != @item.user.id)
+      redirect_to root_path
+    elsif current_user.blank?
+      redirect_to new_user_session_path
+    end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(params[:id])
     else
@@ -41,13 +43,8 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :text, :category_id, :condition_id, :postage_id, :province_id, :number_of_day_id, :price).merge(user_id: current_user.id)
   end
 
-  def remove
+  def identify_item
     @item = Item.find(params[:id])
-    if user_signed_in? && (current_user.id != @item.user.id)
-      redirect_to root_path
-    elsif current_user.blank?
-      redirect_to new_user_session_path
-    end
   end
 
 end

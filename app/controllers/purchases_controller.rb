58842fe1f3af9aa8aa44343579b177aafae83339
KJawoCca <ykariya.pri@gmail.com>
2020-11-02 21:35:53 +0900

    修正4

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,10 +4,10 @@ class PurchasesController < ApplicationController
 
   def index
     @order = Order.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
+    @item = Item.find(params[:item_id])
     @order = Order.new(order_params)
     if @order.valid?
       pay_item
